06122d3778651a9ff427062cbb1ad8ce83440f8d
Nguyen Duc Thai <xichlong.it@gmail.com>
2019-01-08 00:15:02 +0700

    bai 1 tuan 3

---
+++ NguyenDucThai_Bai1_tuan3/NguyenDucThai_Bai1_tuan3/Bai1_tuan3.cpp
@@ -0,0 +1,23 @@
+#include"Header.h"
+
+/*ham xuat ra n so ngau nhien tu 1 den m (n<m)
+INPUT: so lon nhat m
+OUTPUT: n so ngau nhien tu 1 den m
+*/
+void xuatDaySoNN()
+{
+	int m =0;
+	cout << "Nhap vao so lon nhat cua day: "  << endl;
+	cin >> m;
+	int n = rand() % (m - 2) + 1;
+	int a[10];
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() % (m - 1) + 1;
+	}
+	cout << "Day "<<n<<" so ngau nhien tu 1 den " << m << ": ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << " ";
+	}
+}
